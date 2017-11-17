59f00bea822b2956b31a4299027cab414dca0ae4
debeltranc <debeltranc@unal.edu.co>
2017-11-17 09:37:13 -0500

    Update wsgi.py

--- ms_attendance/wsgi.py
+++ ms_attendance/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for ms-attendance project.
+WSGI config for ms_attendance project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ms-attendance.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ms_attendance.settings")
 
 application = get_wsgi_application()
